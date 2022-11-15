7dd2d1a22640009845d1eca0ae98b99a90c9ffb5
office <shrivastavraviprakash526@gmail.com>
2022-11-15 17:07:37 +0530

    Now Holiday

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,58 +1,173 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
-import MaterialTable, { Column } from "@material-table/core";
+import MaterialTable, { column } from "@material-table/core";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 
 const lookup = { true: "Available", false: "Unavailable" };
 
 const columns = [
-  { title: "First Name", field: "firstName" },
-  { title: "Last Name", field: "lastName" },
-  { title: "Birth Year", field: "birthYear", type: "numeric" },
-  { title: "Availablity", field: "availability", lookup }
-];
+  { title: "ID", field: "id" },
+  { title: "TITLE", field: "title" },
+  { title: "DESCRIPTION", field: "description" },
+  { title: "REPORTER", field: "reporter" },
+  { title: "ASSIGNEE", field: "assignee" },
+  { title: "PRIORITY", field: "priority" },
+  { title: "STATUS", field: "status" },
+ ];
+
+ const userColumns = [
+  { title: "ID", field: "id" },
+  { title: "NAME", field: "name" },
+  { title: "EMAIL", field: "email" },
+  { title: "ROLE", field: "userTypes" },
+  { title: "STATUS", field: "status" },
+ ];
 
 const data = [
   { firstName: "Tod", lastName: "Miles", birthYear: 1987, availability: true },
-  { firstName: "Jess", lastName: "Smith", birthYear: 2000, availability: false }
+  {
+    firstName: "Jess",
+    lastName: "Smith",
+    birthYear: 2000,
+    availability: false,
+  },
 ];
 
 // export const App = () => <MaterialTable columns={columns} data={data} />;
 
-function Admin (){
-    return(
-        <div className="bg-light m-5 p-5 vh-100">
-        <Sidebar />
-        <div className="container p-5">
-          <h3 className="text-center text-danger">Welcome Admin !</h3>
-          <p className="text-muted text-center">Take a quick look at your admin stats below</p>
-        </div>
-         {/* widget start */}
-        <div className="row mx-5 mb-2">
-          <div className="col-xs-12 col-lg-3 col-md-6 m-1">
-            <div className="card-shadow bg-primary bg-opacity-50 text-center" style={{
-              width:15 +'rem'}}>
-                <h5 className="card-subtitle my-2 text-primary">
-                  <i className="bi bi-invelope-open text-primary mx-2"></i>
-                  Open
-                </h5>
-                <hr />
-                <div className="row mb-2">
-                  <div className="col text-white mx-4 fw-bolder display-6">
-                    8
-                  </div>
-                  <div className="cd">
-                  <div style={{width:40, height:40}}>
-                  <CircularProgressbar value={8} style={buildStyles({pathColor:"darkblue"})} />
-                  </div>
-                  </div>
+function Admin() {
+  return (
+    <div className="bg-light m-5 p-5 vh-100">
+      <Sidebar />
+      <div className="container p-5">
+        <h3 className="text-center text-danger">Welcome Admin !</h3>
+        <p className="text-muted text-center">
+          Take a quick look at your admin stats below
+        </p>
+      </div>
+      {/* widget start */}
+      <div className="row ms-5 ps-2">
+        <div className="col-xs-2 col-lg-3 col-md-6">
+          <div
+            className="card-shadow bg-primary bg-opacity-75 text-center"
+            style={{
+              width: 15 + "rem",
+            }}
+          >
+            <h5 className="card-subtitle my-2 text-primary">
+              <i className="bi bi-envelope-open text-primary mx-2"></i>
+              OPEN
+            </h5>
+            <hr />
+            <div className="row mb-2">
+              <div className="col text-white mx-4 fw-bolder display-6">8</div>
+              <div className="cd">
+                <div style={{ width: 40, height: 40 }}>
+                  <CircularProgressbar
+                    value={8}
+                    style={buildStyles({ pathColor: "darkblue" })}
+                  />
                 </div>
               </div>
+            </div>
           </div>
         </div>
-            <MaterialTable title="Demo Title" columns={columns} data={data} />
+        {/* widget 2 */}
+        <div className="col-xs-2 col-lg-3 col-md-6">
+          <div
+            className="card-shadow bg-warning bg-opacity-75 text-center"
+            style={{
+              width: 15 + "rem",
+            }}
+          >
+            <h5 className="card-subtitle my-2 text-primary">
+              <i className="bi bi-envelope-open text-primary mx-2"></i>
+              PROGRESS
+            </h5>
+            <hr />
+            <div className="row mb-2">
+              <div className="col text-white mx-4 fw-bolder display-6">20</div>
+              <div className="cd">
+                <div style={{ width: 40, height: 40 }}>
+                  <CircularProgressbar
+                    value={20}
+                    style={buildStyles({ pathColor: "darkgolden" })}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+        {/* Widget 3 */}
+        <div className="col-xs-2 col-lg-3 col-md-6">
+          <div
+            className="card-shadow bg-success bg-opacity-75 text-center"
+            style={{
+              width: 15 + "rem",
+            }}
+          >
+            <h5 className="card-subtitle my-2 text-primary">
+              <i className="bi bi-envelope-open text-primary mx-2"></i>
+              CLOSED
+            </h5>
+            <hr />
+            <div className="row mb-2">
+              <div className="col text-white mx-4 fw-bolder display-6">75</div>
+              <div className="cd">
+                <div style={{ width: 40, height: 40 }}>
+                  <CircularProgressbar
+                    value={75}
+                    style={buildStyles({ pathColor: "darkgreen" })}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* widget 4 */}
+        <div className="col-xs-2 col-lg-3 col-md-6">
+          <div
+            className="card-shadow bg-success bg-opacity-75 text-center"
+            style={{
+              width: 15 + "rem",
+            }}
+          >
+            <h5 className="card-subtitle my-2 text-primary">
+              <i className="bi bi-envelope-open text-primary mx-2"></i>
+              BLOCKED
+            </h5>
+            <hr />
+            <div className="row mb-2">
+              <div className="col text-white mx-4 fw-bolder display-6">36</div>
+              <div className="cd">
+                <div style={{ width: 40, height: 40 }}>
+                  <CircularProgressbar
+                    value={36}
+                    style={buildStyles({ pathColor: "darkgray" })}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> 
+      <div className="container" >
+      <MaterialTable 
+      title="Ticket Details"
+      columns={columns}
+      //  data={data}
+        />
+      </div>
+      <hr />
+      <div className="container" >
+      <MaterialTable 
+      title="User Details"
+      columns={userColumns}
+      //  data={data} 
+      />
+      </div>
+    </div>
+  );
 }
 
 export default Admin;
