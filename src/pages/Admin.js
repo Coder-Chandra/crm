5ace6b8fb012a94858b63a9b7c9412ed47a3afd8
office <shrivastavraviprakash526@gmail.com>
2022-11-16 15:56:00 +0530

    Class 10 Completed

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,7 +1,6 @@
-import { useEffect } from "react";
-import React from "react";
+import { useEffect, useState, React } from "react";
 import Sidebar from "../components/Sidebar";
-import { fetchTicket } from "../api/tickets";
+import { fetchTicket, ticketUpdation } from "../api/tickets";
 import MaterialTable, { column } from "@material-table/core";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import { ExportCsv, ExportPdf } from "@material-table/exporters";
@@ -45,7 +44,47 @@ const data = [
 // export const App = () => <MaterialTable columns={columns} data={data} />;
 
 function Admin() {
-  
+  const [ticketDetails, setTicketDetails] = useState([]);
+  const [ticketStatusCount, setTicketStatusCount] = useState({});
+  useEffect(() => {
+    fetchTickets()
+  }, []);
+
+  const updateTicketCount = (tickets) => {
+    // filling this empty object with the ticket counts
+    // Segrating the tickets in 4 properties according to the status of the tickets
+    const data = {
+      open: 0,
+      closed: 0,
+      progress: 0,
+      blocked: 0,
+    };
+
+    tickets.forEach((x) => {
+      if (x.status === "OPEN") {
+        data.open += 1;
+      } else if (x.status === "CLOSED") {
+        data.closed += 1;
+      } else if (x.status === "IN_PROGRESS") {
+        data.progress += 1;
+      } else {
+        data.blocked += 1;
+      }
+    });
+
+    setTicketStatusCount(Object.assign({}, data));
+  };
+
+  const fetchTickets = () => {
+    fetchTicket()
+      .then((response) => {
+        setTicketDetails(response.data);
+        updateTicketCount(response.data);
+      }).catch (function (error){
+      console.log(error);
+    })
+    setTicketStatusCount(Object.assign({},data))
+  }
   return (
     <div className="bg-light m-5 p-5 vh-100">
       <Sidebar />
@@ -91,7 +130,7 @@ function Admin() {
             }}
           >
             <h5 className="card-subtitle my-2 text-primary">
-              <i className="bi bi-envelope-open text-primary mx-2"></i>
+              <i className="bi bi-hourglass-split text-primary mx-2"></i>
               PROGRESS
             </h5>
             <hr />
@@ -117,7 +156,7 @@ function Admin() {
             }}
           >
             <h5 className="card-subtitle my-2 text-primary">
-              <i className="bi bi-envelope-open text-primary mx-2"></i>
+              <i className="bi bi-check2-circle text-primary mx-2"></i>
               CLOSED
             </h5>
             <hr />
@@ -137,13 +176,13 @@ function Admin() {
         {/* widget 4 */}
         <div className="col-xs-2 col-lg-3 col-md-6">
           <div
-            className="card-shadow bg-success bg-opacity-75 text-center"
+            className="card-shadow bg-secondary bg-opacity-75 text-center"
             style={{
               width: 15 + "rem",
             }}
           >
             <h5 className="card-subtitle my-2 text-primary">
-              <i className="bi bi-envelope-open text-primary mx-2"></i>
+              <i className="bi bi-slash-circle text-primary mx-2"></i>
               BLOCKED
             </h5>
             <hr />
@@ -162,29 +201,37 @@ function Admin() {
         </div>
       </div> 
       <div className="container" >
-      <MaterialTable 
-      title="Ticket Details"
-      columns={columns}
-      options = {{filtering : true,
-        exportMenu : [{
-            label : "Export Pdf",
-            exportFunc : ( cols, data) => ExportPdf(cols, data, "userRecords")
-          },
-          {
-            label : "Export Csv",
-            exportFunc : ( cols, data) => ExportCsv(cols, data, "userRecords")
-          }],
-      headerStyle : {
-        backgroundColor : "#d9534f",
-        color : "#fff"},
-        rowStyle : {
-          backgroundColor : "eee"}
-      }}
-      //  data={data}
+      <MaterialTable
+          // 1. grabbing the specific ticket from the row
+          // onRowClick={(event, rowData) => editTicket(rowData)}
+          title="TICKET"
+          columns={columns}
+          data={ticketDetails}
+          options={{
+            filtering: true,
+            headerStyle: {
+              backgroundColor: "#d9534f",
+              color: "#fff",
+            },
+            rowStyle: {
+              backgroundColor: "#eee",
+            },
+
+            exportMenu: [
+              {
+                label: "Export Pdf",
+                exportFunc: (cols, data) =>
+                  ExportPdf(cols, data, "ticketRecords"),
+              },
+              {
+                label: "Export Csv",
+                exportFunc: (cols, data) =>
+                  ExportCsv(cols, data, "ticketRecords"),
+              },
+            ],
+          }}
         />
-      </div>
-      <hr />
-      <div className="container" >
+        <hr />
       <MaterialTable 
       title="User Details"
       columns={userColumns}
